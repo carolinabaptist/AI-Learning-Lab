1a67038def3b96bada34857473fb829340b5adf3
girlfromrio <acarolbaptista1@gmail.com>
2025-02-11 10:31:52 +0000

    New files and folders

---
+++ main.js
@@ -0,0 +1,23 @@
+/*responsável por inicializar o Phaser e registrar todas as cenas do jogo*/
+
+import Phaser from "phaser";
+import IntroScene from './src/scenes/IntroScene.js';
+import LanguageScene from './src/scenes/LanguageMenu.js';
+import MapScene from './src/scenes/MapScene.js';
+import MiniGame1Scene from './src/scenes/Game1Scene.js';
+import MiniGame2Scene from './src/scenes/Game2Scene.js';
+
+const config = {
+    
+    width: window.innerWidth,
+    height: window.innerHeight,
+    
+    scene: [ // Adiciona todas as cenas ao jogo
+        IntroScene,
+        LanguageScene,
+        MapScene,
+        MiniGame1Scene,
+        MiniGame2Scene
+    ]
+};
+
