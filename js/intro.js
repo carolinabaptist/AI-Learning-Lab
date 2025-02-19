fac275cc1f1a8787d81280ad2892a1188067e15f
girlfromrio <acarolbaptista1@gmail.com>
2025-02-19 18:33:01 +0000

    new help button in the intro

---
+++ js/intro.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const helpButton = document.querySelector(".help-container"); // Captura a DIV do botão de ajuda
+    const messageBoard = document.getElementById("message-board");
+    const closeButton = document.getElementById("close-button");
+
+    // Mostrar o quadro de mensagem ao clicar no botão de ajuda
+    helpButton.addEventListener("click", function() {
+        messageBoard.classList.remove("hidden");
+    });
+
+    // Esconder o quadro de mensagem ao clicar no botão OK
+    closeButton.addEventListener("click", function() {
+        messageBoard.classList.add("hidden");
+    });
+});
