035dd10e7fe89572fb84d73750ac7972e4534edd
girlfromrio <acarolbaptista1@gmail.com>
2025-03-12 15:38:59 +0000

    Add modojogo variable to guide game according to the choice of players

--- js/mini-jogo-2.js
+++ js/mini-jogo-2.js
@@ -1,1 +1,14 @@
+document.querySelectorAll(".pixel-button").forEach((button) => {
 
+    button.addEventListener("click", () => {
+        let modojogo;
+
+        if (button.textContent === "1"){
+            modojogo = 1;
+        } else if (button.textContent === "2"){
+            modojogo = 2;
+        }
+
+        console.log("Modo de jogo selecionado:", modojogo);
+    });
+});
