d2ca309c76a206e921bef9976da7900615e8d29f
girlfromrio <acarolbaptista1@gmail.com>
2025-03-13 19:38:02 +0000

    Will use google voice

--- js/common.js
+++ js/common.js
@@ -1,5 +1,24 @@
 const $ = (x) => document.querySelectorAll(x);
 
-function playSound() {
-    $('#click-sound')[0].play();
+function sliceEmoji(str) {
+    let res = ['', ''];
+
+    for (let c of str) {
+        let n = c.codePointAt(0);
+        let isEmoji = n > 0xfff || n === 0x200d || (0xfe00 <= n && n <= 0xfeff);
+        res[1 - isEmoji] += c;
+    }
+    return res;
+}
+
+function hex(str) {
+    return [...str].map(x => x.codePointAt(0).toString(16));
+}
+
+function emojiToCodePoints(emoji) {
+    return sliceEmoji(emoji).map(hex);
+}
+
+function emojiURL(emoji) {
+    return "https://emojicdn.elk.sh/" + emojiToCodePoints(emoji).join('-') + "png";
 }
