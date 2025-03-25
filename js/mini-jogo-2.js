fef5f1ca9470fda466bd0eb6294ac545fb0fae4b
girlfromrio <acarolbaptista1@gmail.com>
2025-03-25 19:56:06 +0000

    remove greeting

--- js/mini-jogo-2.js
+++ js/mini-jogo-2.js
@@ -34,11 +34,6 @@ let jogador = "";
 let pontuacao1 = 0;
 let pontuacao2 = 0;
 
-function iniciarSaudacao() {
-    const saudacao = `Bem-vindos à corrida de robôs! Hoje na pista temos ${jogador1} contra ${jogador2}. Que vença quem conseguir montar o caminho mais rápido!`;
-    falar(saudacao);
-}
-
 function iniciarJogo() {
 
     /*jogando sozinha*/
@@ -51,8 +46,6 @@ function iniciarJogo() {
 
     document.getElementById("tela-nomes").style.display = "none";
     document.getElementById("placar").style.display = "block";
-
-    iniciarSaudacao();
 }
 
 function tocarSomChegada() {
@@ -71,7 +64,7 @@ function falar(texto) {
 document.addEventListener("DOMContentLoaded", function () {
     let score1 = 0;
     let score2 = 0;
-    const maxRounds = 3;
+    const maxRounds = 2;
     let round = 1;
     let keyPressCount = 0;
     const score1Display = document.getElementById("score1");
@@ -89,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(`Round: ${round}, Max Rounds: ${maxRounds}`);
 
         keyPressCount++;
-        if (keyPressCount === 2 || keyPressCount === 4 || keyPressCount ==6) return;
+        if (keyPressCount === 2 || keyPressCount === 4) return;
         if (event.key === "1") {
                 score1++;
                 score1Display.textContent = score1;
@@ -100,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     setTimeout(() => {
                         placar.style.display = "none";
                         msg.style.display = "block";
-                    }, 3000);
+                    }, 4000);
                 }
                 return
         } else if (event.key === "2") {
@@ -113,9 +106,179 @@ document.addEventListener("DOMContentLoaded", function () {
                     setTimeout(() => {
                         placar.style.display = "none";
                         msg.style.display = "block";
-                    }, 3000);
+                    }, 4000);
                 }
                 return
         }
     });
+});
+
+const waitTimeCharacter = 60;
+const waitTimeParagraph = 200;
+ 
+let currentPage = null;
+ 
+let textEnded = false;
+let speechEnded = false;
+ 
+let speechFn = null;
+ 
+function removeEmoji(text) {
+    return text.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ''); 
+}
+ 
+function shouldSpeak() {
+    let screen = $('#typewriter-screen')[0];
+    return screen.classList.contains('typewriter-speech');
+}
+ 
+function speakText(originalText) {
+    const text = removeEmoji(originalText);
+ 
+ 
+    console.log('aaa', originalText, text, speechEnded);
+    let speech = new SpeechSynthesisUtterance(text);
+ 
+    speech.lang = "EN-US"
+ 
+    if (shouldSpeak()) {
+        speech.volume = 3;
+    }
+    else {
+        speech.volume = 0;
+    }
+ 
+    speech.rate = 0.7;
+    speech.pitch = 3;
+ 
+ 
+    speech.addEventListener('end', (event) => {
+        console.log('Speech ended after', event.elapsedTime, 'ms');
+        speechEnded = true;
+ 
+        if (speechFn != null) {
+            speechFn();
+            speechFn = null;
+        }
+    });
+ 
+    speechSynthesis.speak(speech);
+}
+ 
+function typewriterStep(playingPage, playingParagraph, i) {
+    const pages = $('#typewriter-screen .typewriter-page');
+    const page = pages[playingPage];
+    const paragraphs = page.querySelectorAll('.typewriter-effect');
+    const paragraph = paragraphs[playingParagraph];
+    const text = paragraph.getAttribute('typewriter-text');
+ 
+    textEnded = false;
+ 
+    if (playingPage == 0 && playingParagraph == 0 && i == 0) {
+        $('.typewriter-continue')[0].textContent = 'CONTINUE';
+    }
+ 
+    if (playingPage != currentPage) {
+        return;
+    }
+ 
+    //if (playingParagraph == 0 && i == 0) {
+    //    speakText(text);
+    //}
+ 
+    if (i < text.length) {
+        paragraph.innerText = text.slice(0, i + 1);
+ 
+        setTimeout(() => typewriterStep(playingPage, playingParagraph, i + 1), waitTimeCharacter);
+    }
+    else if (playingParagraph + 1 < paragraphs.length) {
+        textEnded = true;
+ 
+        const nextParagraph = () => {
+            textEnded = false;
+            speechEnded = false;
+ 
+            const nextParagraph = playingParagraph + 1;
+            const nextText = paragraphs[nextParagraph].getAttribute('typewriter-text');
+ 
+            speakText(nextText);
+            typewriterStep(playingPage, nextParagraph, 0);
+        };
+ 
+        if (speechEnded) {
+            setTimeout(nextParagraph, waitTimeParagraph);
+        }
+        else {
+            speechFn = nextParagraph;
+        }
+    }
+}
+ 
+function typewriterContinue() {
+    const pages = $('#typewriter-screen .typewriter-page');
+ 
+    if (currentPage == null) {
+        currentPage = 0;
+    }
+    else {
+        const previousPage = pages[currentPage];
+        previousPage.classList.add('disabled');
+        window.speechSynthesis.cancel();
+        speechEnded = false;
+
+        currentPage++;
+    }
+ 
+    const page = pages[currentPage];
+    page.classList.remove('disabled');
+ 
+    if (currentPage + 1 >= pages.length) {
+        $('.typewriter-continue')[0].classList.add('disabled');
+        $('.typewriter-end')[0].classList.remove('disabled');
+    }
+ 
+    const text = page.querySelector('.typewriter-effect').getAttribute('typewriter-text');
+    console.log('Speaking:', text, window.speechSynthesis.getVoices().length);
+ 
+    speakText(text);
+ 
+    typewriterStep(currentPage, 0, 0);
+}
+ 
+function begin() {
+    document.addEventListener('keydown', (event) => {
+        if (event.key == ' ') {
+            $('.pixel-button:not(.disabled)')[0].click();
+        }
+    });
+ 
+    if (!shouldSpeak()) {
+        typewriterContinue();
+    }
+}
+ 
+window.addEventListener('load', () => {
+    let screen = $('#typewriter-screen')[0];
+    console.log(screen);
+ 
+    if (!screen) {
+        console.log("We don't have a typewriteer effect here");
+        return;
+    }
+ 
+    for (const paragraph of $('#typewriter-screen .typewriter-page .typewriter-effect')) {
+        const text = paragraph.innerText;
+        paragraph.innerText = '';
+        paragraph.setAttribute('typewriter-text', text);
+    }
+ 
+    if (window.speechSynthesis.getVoices().length == 0) {
+        console.log('Waiting for voices to load');
+        window.speechSynthesis.addEventListener('voiceschanged', () => {
+                begin();
+        });
+    }
+    else {
+        begin();
+    }
 });
