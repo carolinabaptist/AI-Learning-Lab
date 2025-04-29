85e60409d09bf78ec18a1256478c180561e64fbd
girlfromrio <acarolbaptista1@gmail.com>
2025-04-29 19:56:17 +0100

    Mario is moving

--- js/mini-jogo-3.js
+++ js/mini-jogo-3.js
@@ -1,4 +1,15 @@
 document.getElementById("tela-jogo").style.display = "none";
+document.getElementById("tela-final").style.display = "none";
+
+// the idea is to be able to change those values in the browser console
+
+var scrollVal = 0;
+var marioPos = 200;
+var marioSpeed = 10;
+
+var leftPressed = false;
+var rightPressed = false;
+var upPressed = false;
 
 const canvas = document.getElementById("jogo-canvas");
 const ctx = canvas.getContext("2d");
@@ -14,9 +25,6 @@ const backgroundScale = backgroundHeight / canvasHeight;
 
 const backgroundShow = canvasWidth / backgroundScale;
 
-
-const scrollVal = 0;
-
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
@@ -24,6 +32,8 @@ canvas.height = canvasHeight;
 // Cria uma nova imagem
 const background = new Image();
 background.src = "../../assets/images/mario-bc.png";
+const spriteMario = new Image();
+spriteMario.src = "../../assets/images/sprite-sheet-mario.png";
 
 // carrega imagem de fundo no canvas
 async function carregarBg() {
@@ -48,9 +58,6 @@ async function carregarModelo() {
     const weightsBin = document.getElementById("weightsBin").files[0];
     const metadataJson = document.getElementById("metadataJson").files[0];
 
-
-    
-
     // load the model and metadata
     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
     // or files from your local hard drive
@@ -64,9 +71,8 @@ async function carregarModelo() {
     await webcam.setup(); // request access to the webcam
     await webcam.play();
 
-    //await carregarBg();
 
-    window.requestAnimationFrame(loop);
+    //await carregarBg();
 
     // append elements to the DOM
     document.getElementById("webcam-container").appendChild(webcam.canvas);
@@ -75,24 +81,96 @@ async function carregarModelo() {
         labelContainer.appendChild(document.createElement("div"));
     }
 
+    
+    window.requestAnimationFrame(loop);
 }
 
-async function loop() {
-    webcam.update(); // update the webcam frame
-    let prediction = await predict();
 
-    let predictedClass = getClass(prediction);
+document.getElementById("tela-inicial").style.display = "none";
+document.getElementById("tela-jogo").style.display = "block";
+window.requestAnimationFrame(loop);
+
+async function loop() {
+    //webcam.update(); // update the webcam frame
+    //let prediction = await predict();
+
+    //let predictedClass = getClass(prediction);
 
     //console.log(predictedClass);
 
-    //console.log("vou desenhar");
+    //console.log("vou desenhar");´
 
-    ctx.drawImage(background, scrollVal, 0, scrollVal, backgroundHeight);
+    //console.log(scrollVal);
 
+    if (leftPressed) {
+        move(-marioSpeed);
+    }
+    else if (rightPressed) {
+        move(marioSpeed);
+    }
+
+    ctx.drawImage(background, scrollVal * backgroundScale, 0, canvasWidth * backgroundScale, backgroundHeight, 0, 0, canvasWidth, canvasHeight);
+
+    ctx.drawImage(spriteMario, 80, 32, 16, 16, marioPos - scrollVal, canvasHeight - 40 / backgroundScale, 16 / backgroundScale, 16 / backgroundScale);
     //console.log("ok");
+
+    if (marioPos >= 10920) {
+        console.log("ganhou");
+    }
 
     window.requestAnimationFrame(loop);
 }
+
+function scroll(amount) {
+    scrollVal += amount;
+
+    if (scrollVal < 0) {
+        scrollVal = 0;
+    }
+
+    if (scrollVal > backgroundWidth / backgroundScale) {
+        scrollVal = backgroundWidth / backgroundScale;
+    }
+}
+
+function move(amount) {
+    const scrollBorder = 150;
+    const scrollAmount = scrollBorder + 100;
+    const screenMario = marioPos - scrollVal;
+
+    marioPos += amount;
+
+    if (marioPos < 0) {
+        marioPos = 0;
+    }
+
+    if (screenMario < scrollBorder) { 
+        scroll(-scrollAmount);
+    }
+    if (screenMario > canvasWidth - scrollBorder - 16 / backgroundScale) {
+        scroll(scrollAmount);
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        rightPressed = true;
+    } else if (event.key === "ArrowLeft") {
+        leftPressed = true;
+    } else if (event.key === "ArrowUp") {
+        upPressed = true;
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    if (event.key === "ArrowRight") {
+        rightPressed = false;
+    } else if (event.key === "ArrowLeft") {
+        leftPressed = false;
+    } else if (event.key === "ArrowUp") {
+        upPressed = false;
+    }
+});
 
 function getClass(prediction) {
     let maxProb = 0;
@@ -118,36 +196,3 @@ async function predict() {
 
     return prediction;
 }
-
-
-//let model;
-/*
-async function carregarModelo_() {
-
-    document.getElementById("tela-inicial").style.display = "none";
-    document.getElementById("tela-jogo").style.display = "block";
-
-    const modelJson = document.getElementById("modelJson").files[0];
-    const weightsBin = document.getElementById("weightsBin").files[0];
-    const metadataJson = document.getElementById("metadataJson").files[0];
-
-    try {
-        // Carrega o modelo
-        const handler = tf.io.browserFiles([modelJson, weightsBin]);
-        model = await tf.loadLayersModel(handler);
-        console.log("✅ Modelo carregado com sucesso!");
-
-        // Lê o metadata (opcional, mas útil)
-        const reader = new FileReader();
-        reader.onload = function (event) {
-            const metadata = JSON.parse(event.target.result);
-            console.log("🧠 Metadata carregado:", metadata);
-        };
-        reader.readAsText(metadataJson);
-        
-    } catch (error) {
-        console.error("❌ Erro ao carregar o modelo:", error);
-        alert("Erro ao carregar o modelo.");
-    }
-}
-*/
