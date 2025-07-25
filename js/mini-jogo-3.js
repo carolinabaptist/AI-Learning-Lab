16ea3c176fd1a81d854d0fec3ddf8c67df156192
girlfromrio <acarolbaptista1@gmail.com>
2025-07-25 17:02:37 +0100

    mario jumping a little bit faster

--- js/mini-jogo-3.js
+++ js/mini-jogo-3.js
@@ -59,7 +59,7 @@ function init() {
     marioPosx = 200;
     marioPosy = -12.8; // was 0;
     marioSpeed = 400;
-    marioJumpSpeed = 950;
+    marioJumpSpeed = 1100;
     marioJumpTime = 0.3; // seconds
     marioFacingRight = true;
     marioWalking = false;
